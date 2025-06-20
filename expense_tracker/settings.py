6eeff1f155199d6188da557bce2fea80277bec9d
Davisetims <etimsintegration@gmail.com>
2025-06-20 15:52:07 +0300

    `Added PostgreSQL database configuration with environment variable fallback and local override`

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -126,13 +126,24 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
+POSTGRES_LOCALLY = False
+
+if DEBUG == False or POSTGRES_LOCALLY == True:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL')
+        )
+
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
