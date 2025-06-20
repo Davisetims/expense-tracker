5c129bee52107e324c13b83c9d794a6dcb2e7e12
Mwema22 <stacybeldina50@gmail.com>
2025-06-20 15:54:55 +0300

    admin setup

--- expenditure/admin.py
+++ expenditure/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from expenditure.models import Category, Budget, Expenditure, Transaction
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Budget)
+admin.site.register(Expenditure)
+admin.site.register(Transaction)
+
+
